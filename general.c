6bce7da72ad49dd9f4fa4ff49c43a34a826c242d
The Water Goebblin <thewatergoebblin@gmail.com>
2017-12-28 17:48:47 +0000

    adding hash map code.

--- general.c
+++ general.c
@@ -1,5 +1,93 @@
 #include "general.h"
 #include <stdio.h>
+#include <string.h>
+
+struct map_record_s {
+	const char *key;
+	void *value;
+	map_record_s *next;
+};
+
+static unsigned pjw_hash(const char *key);
+
+map_s *map_alloc(void) {
+	return sa_allocz(sizeof(map_s));
+}
+
+int map_insert(map_s *map, const char *key, void *value) {
+	map_record_s **prec = &map->table[pjw_hash(key)],
+				 *rec = *prec,
+				 *nrec;
+	nrec = sa_alloc(sizeof *nrec);
+	nrec->key = key;
+	nrec->value = value;
+	nrec->next = NULL;
+	if(rec) {
+		while(rec->next) {
+			if(!strcmp(rec->key, key)) {
+				free(nrec);
+				return -1;
+			}
+			rec = rec->next;
+		}
+		if(!strcmp(rec->key, key)) {
+			free(nrec);
+			return -1;
+		}
+		rec->next = nrec;
+	}
+	else {
+		*prec = nrec;
+	}
+	return 0;
+}
+
+void *map_get(map_s *map, const char *key) {
+	map_record_s *rec = map->table[pjw_hash(key)];
+
+	while(rec) {
+		if(!strcmp(rec->key, key)) {
+			return rec->value;
+		}
+		rec = rec->next;
+	}
+	return NULL;
+}
+
+int map_delete(map_s *map, const char *key) {
+	map_record_s **prec = &map->table[pjw_hash(key)],
+				 *rec = *prec, *bptr;
+
+	for(bptr = rec; rec; rec = rec->next) {
+		if(!strcmp(rec->key, key)) {
+			if(rec == bptr) {
+				*prec = rec->next;
+			}
+			else {
+				bptr->next = rec->next;
+			}
+			free(rec);
+			return 0;
+		}
+		bptr = rec;
+	}
+	return -1;
+}
+
+void map_dealloc(map_s *map) {
+	int i;
+	map_record_s *rec, *bptr;
+
+	for(i = 0; i < HASH_TABLE_SIZE; i++) {
+		rec = map->table[i];
+		while(rec) {
+			bptr = rec->next;
+			free(rec);
+			rec = bptr;
+		}
+	}
+	free(map);
+}
 
 void *sa_alloc(size_t size) {
 	void *p = malloc(size);
@@ -28,4 +116,14 @@ void *sa_ralloc(void *ptr, size_t size) {
 	return p;
 }
 
+unsigned pjw_hash(const char *key) {
+	unsigned h = 0, g;
+	while(*key) {
+		h = (h << 4) + *key++;
+		if((g = h & (unsigned)0xF0000000) != 0) {
+			h = (h ^ (g >> 4)) ^g;
+		}
+	}
+	return (unsigned)(h % HASH_TABLE_SIZE);
+}
 
--- general.h
+++ general.h
@@ -4,6 +4,20 @@
 #include <stdlib.h>
 
 #define ERR_BUF_SIZE 512
+#define HASH_TABLE_SIZE 97
+
+typedef struct map_s map_s;
+typedef struct map_record_s map_record_s;
+
+struct map_s {
+	int size;
+	map_record_s *table[HASH_TABLE_SIZE]; };
+
+extern map_s *map_alloc(void);
+extern int map_insert(map_s *map, const char *key, void *value);
+extern void *map_get(map_s *map, const char *key);
+extern int map_delete(map_s *map, const char *key);
+extern void map_dealloc(map_s *map);
 
 extern void *sa_alloc(size_t size);
 extern void *sa_allocz(size_t size);
