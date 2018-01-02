e28c7d11e22183da5686d2c87f0f264e3fcd7355
The Water Goebblin <thewatergoebblin@gmail.com>
2018-01-02 23:18:24 +0000

    parsing map tiles

--- sa-map.c
+++ sa-map.c
@@ -5,6 +5,7 @@
 #include <ctype.h>
 #include <stdbool.h>
 
+#define INITIAL_TILE_LIST_SIZE 512
 #define MAX_LEX_LEN 128
 
 typedef enum map_token_e map_token_e;
@@ -31,6 +32,8 @@ static int map_next_tok(map_token_s *tok, char **ptr);
 static int map_token_init(map_token_s *tok, char *lexeme, int len, map_token_e type);
 static int sa_build_map(sa_map_s *map, char **ptr);
 static int sa_parse_heading(sa_map_s *map, char **ptr);
+static int sa_parse_tiles(sa_map_s *map, char **ptr);
+static int sa_parse_tile_list(sa_map_s *map, char **ptr);
 
 int sa_parse_map(sa_map_s *map, buf_s src) {
 	map_token_s *tok;		
@@ -120,6 +123,10 @@ int sa_build_map(sa_map_s *map, char **ptr) {
 	result = sa_parse_heading(map, ptr);
 	if(result) 
 		return -1;
+	result = sa_parse_tiles(map, ptr);
+	if(result)
+		return -1;
+	return 0;
 }
 
 int sa_parse_heading(sa_map_s *map, char **ptr) {
@@ -183,5 +190,75 @@ int sa_parse_heading(sa_map_s *map, char **ptr) {
 		log_error("Syntax Error parsing sa map in %s(): unexpected token: %s", __func__, tok.lex);
 		return -1;
 	}
+}
+
+int sa_parse_tiles(sa_map_s *map, char **ptr) {
+	int result;
+	map_token_s tok;
+
+	result = map_next_tok(&tok, ptr);
+	if(result)
+		return -1;
+	if(tok.type == MAP_TOK_IDENT && !strcmp(tok.lex, "tiles")) {
+		result = map_next_tok(&tok, ptr);
+		if(result)
+			return -1;
+		if(tok.type != MAP_TOK_EQ) {
+			log_error("Syntax Error parsing sa map in %s(): expected '=' but got %s.", __func__, tok.lex);
+			return -1;
+		}
+		return sa_parse_tile_list(map, ptr);
+	}
+	else {
+		log_error("Syntax Error parsing sa map in %s(): unexpected token: %s", __func__, tok.lex);
+		return -1;
+	}
+}
+
+int sa_parse_tile_list(sa_map_s *map, char **ptr) {
+	int tile_list_size = 0, 
+		tile_list_bsize = INITIAL_TILE_LIST_SIZE,
+		result;
+	map_token_s tok;
+	sa_tile_s tile, *tile_buffer;
+	
+	tile_buffer = sa_alloc(tile_list_bsize * sizeof(*tile_buffer));
+	while(true) {
+		result = map_next_tok(&tok, ptr);
+		if(result == -1) {
+			free(tile_buffer);
+			return -1;
+		}
+		if(tok.type == MAP_TOK_AMP) {
+			int inf_product = map->infx * map->infy;
+			if(tile_list_size != inf_product) {
+				log_error("Unexpected tile list size in parsed map in %s(). Expected %d, but got %d tiles.", __func__, inf_product, tile_list_size);
+				free(tile_buffer);
+				return -1;
+			}
+			tile_buffer = sa_ralloc(tile_buffer, tile_list_size * sizeof(*tile_buffer));
+			map->tiles = tile_buffer;
+			return 0;
+		}
+		else if(tok.type == MAP_TOK_IDENT) {
+			if(tok.len >= SA_TILE_LEN) {
+				log_error("Error parsing map file in %s(): tile too large: %s", __func__, tok.lex);
+				free(tile_buffer);
+				return -1;
+			}
+			if(tile_list_size == tile_list_bsize) {
+				tile_list_bsize *= 2;
+				tile_buffer = sa_ralloc(tile_buffer, tile_list_bsize * sizeof(*tile_buffer));
+			}
+			strcpy(tile_buffer[tile_list_size].id, tok.lex);
+			tile_list_size++;
+		}
+		else {
+			log_error("Syntax Error parsing sa map in %s(): expected identifier but got %s", __func__, tok.lex);
+			free(tile_buffer);
+			return -1;
+		}
+	}	
+
 }
 
--- sa-map.h
+++ sa-map.h
@@ -3,14 +3,20 @@
 
 #include "general.h"
 
+#define SA_TILE_LEN 8
 #define MAX_ARENA_NAME_SIZE 64
 
+typedef struct sa_tile_s sa_tile_s;
 typedef struct sa_map_s sa_map_s;
+
+struct sa_tile_s {
+	char id[SA_TILE_LEN];
+};
 
 struct sa_map_s {
 	int infx, infy;
 	char name[MAX_ARENA_NAME_SIZE];
-	buf_s tiles;
+	sa_tile_s *tiles;
 	buf_s sp;
 	buf_s ws;
 	int rt;
