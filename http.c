2816d1def5b4a9c25113404fed0711b61bdd810f
The Water Goebblin <thewatergoebblin@gmail.com>
2018-01-15 00:49:26 -0600

    Adding port and path parsing for http.

--- http.c
+++ http.c
@@ -12,6 +12,7 @@
 #define MAX_PORT 8
 #define MAX_URI_PATH 128
 #define URI_BUF_SIZE 512
+#define URI_MAX_PORT 65535
 
 typedef enum uri_tok_type_e uri_tok_type_e;
 
@@ -65,7 +66,8 @@ static int http_syntax_uri(uri_s *uri, uri_toklist_s *list);
 static int http_syntax_scheme(uri_s *uri, uri_tok_s **tok);
 static int http_syntax_user_info(uri_s *uri, uri_tok_s **tok);
 static int http_syntax_host(uri_s *uri, uri_tok_s **tok);
-static int http_host_is_ipv4(char *lex);
+static bool http_host_is_ipv4(char *lex);
+static int http_syntax_port(uri_s *uri, uri_tok_s **tok);
 
 void http_test(char *uri) {
     int result;
@@ -301,7 +303,24 @@ int http_syntax_uri(uri_s *uri, uri_toklist_s *list) {
     if(result == -1) {
         return -1;
     }
-
+    result = http_syntax_port(uri, &tok);
+    if(result == -1) {
+        return -1;
+    }
+    if(tok->type == URI_SEGMENT) {
+        if(tok->len < MAX_URI_PATH) {
+            strcpy(uri->path, tok->lex);
+            tok = tok->next;
+        }
+        else {
+            log_error("Error parsing URI in %s(): URI path too long: %s.", __func__, tok->lex);
+            return -1;
+        }
+    }
+    if(tok->type != URI_END) {
+        log_error("Syntax Error parsing URI in %s(): Unexpected token: %s.", __func__, tok->lex);
+        return -1;
+    }
     return 0;
     
 }
@@ -378,19 +397,23 @@ int http_syntax_host(uri_s *uri, uri_tok_s **tok) {
         if(t->len < MAX_HOST) {
             strcpy(uri->host, t->lex);
             uri->is_ipv4 = http_host_is_ipv4(t->lex);
+            *tok = t->next;
+            return 0;
         }
         else {
             log_error("Error parsing URI in %s(): Host name too long: %s", __func__, t->lex);
+            *tok = t->next;
             return -1;
         }
     }
     else {
         log_error("Syntax Error parsing URI in %s(): Expected text sequence for host, but got %s.", __func__, t->lex);
+        *tok = t->next;
         return -1;
     }
 }
 
-int http_host_is_ipv4(char *lex) {
+bool http_host_is_ipv4(char *lex) {
     int octet, i;
     char *bptr;
     
@@ -414,5 +437,55 @@ int http_host_is_ipv4(char *lex) {
         }
     }
     return true;
+}
+
+int http_syntax_port(uri_s *uri, uri_tok_s **tok) {
+    int port;
+    char *ptr, bck;
+    uri_tok_s *t = *tok;
+    
+    if(t->type == URI_COLON) {
+        t = t->next;
+        if(t->type == URI_SEGMENT) {
+            ptr = t->lex;
+            while(*ptr) {
+                if(!isdigit(*ptr)) {
+                    if(*ptr == '/') {
+                        break;
+                    }
+                    else {
+                        log_error("Error parsing URI in %s() - invalid port number: %s.", __func__, t->lex);
+                        *tok = t->next;
+                        return -1;
+                    }
+                }
+                ptr++;
+            }
+            bck = *ptr;
+            *ptr = '\0';
+            port = atoi(t->lex);
+            *ptr = bck;
+            if(port >= 0 && port <= URI_MAX_PORT) {
+                uri->port = port;
+                //transforming token (hacky solution to awkward parsing)
+                t->len = strlen(ptr) + 1;
+                memmove(t->lex, ptr, t->len);
+                *tok = t;
+                return 0;
+            }
+            else {
+                log_error("Error parsing URI in %s() - port number out of range: %d", __func__, port);
+                *tok = t->next;
+                return -1;
+            }
+        }
+        else {
+            log_error("Syntax Error in URI in %s(): Expected digit sequence, but got %s.", __func__, t->lex);
+            *tok = t->next;
+            return -1;
+        }
+    }
+    *tok = t->next;
+    return 0;
 }
 
