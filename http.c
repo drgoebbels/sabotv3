1f94611d39cb39ffc95618ac8ed0082779f4a636
The Water Goebblin <thewatergoebblin@gmail.com>
2018-01-14 22:51:37 -0600

    fixing http.c indentation

--- http.c
+++ http.c
@@ -27,10 +27,10 @@ enum uri_tok_type_e {
 };
 
 struct uri_s {
-	char scheme[MAX_URI_SCHEME];
-	char user_info[MAX_USERINFO]; 
-	char host[MAX_HOST];
-	int port;
+    char scheme[MAX_URI_SCHEME];
+    char user_info[MAX_USERINFO]; 
+    char host[MAX_HOST];
+    int port;
     char path[MAX_URI_PATH];
 };
 
@@ -47,7 +47,7 @@ struct uri_toklist_s {
 };
 
 static const char *supported_methods[] = {
-	"GET", "POST"
+    "GET", "POST"
 };
 
 static int http_parse_request_line(http_request_s *req, char **ptr);
@@ -63,7 +63,7 @@ void http_test(char *uri) {
     int result;
     uri_s u;
 
-	log_info("parsing uri: %s", uri);
+    log_info("parsing uri: %s", uri);
     result = http_parse_uri(&u, uri);
     if(result) {
         log_error("parse test failed");
@@ -79,108 +79,108 @@ void http_test(char *uri) {
 }
 
 int http_parse_request(http_request_s *req, char *raw) {
-	int result;
+    int result;
 
-	result = http_parse_request_line(req, &raw);
-	if(result) 
-		return -1;
-	log_info("method: %s uri: %s version: %s", req->method, req->uri, req->version);
-	return 0;
+    result = http_parse_request_line(req, &raw);
+    if(result) 
+        return -1;
+    log_info("method: %s uri: %s version: %s", req->method, req->uri, req->version);
+    return 0;
 }
 
 int http_parse_request_line(http_request_s *req, char **ptr) {
-	int i;
-	buf_s *uri = &req->uri;
-	char *fptr = *ptr;
-	char *method = req->method;
-	char *version = req->version;
-		
-	for(i = 0; i < MAX_METHOD_LEN; i++) {
-		if(*fptr == ' ') {
-			method[i] = '\0';
-			if(!http_check_method(method)) {
-				log_error("HTTP Unsupported Request method: %s", method);
-				return HTTP_UNSUPPORTED_METHOD;
-			}
-			fptr++;
-			break;
-		}
-		method[i] = *fptr++;
-	}
-	if(i == MAX_METHOD_LEN) {
-		method[i - 1] = '\0';
-		log_error("Request Method too long: %s", method);
-		return HTTP_BAD_REQUEST;
-	}
-	buf_init(uri);
-	for(i = 0; i < MAX_URI_LEN; i++) {
-		if(*fptr == ' ') {
-			buf_add_char(uri, '\0');
-			fptr++;
-			break;
-		}
-		buf_add_char(uri, *fptr);
-		fptr++;
-	}
-	if(i == MAX_URI_LEN) {
-		buf_add_char(uri, '\0');
-		log_error("URI too long: %s", uri->data);
-		return HTTP_BAD_REQUEST;
-	}
-	for(i = 0; i < MAX_VERSION_LEN; i++) {
-		if(*fptr == '\r') {
-			if(*(fptr + 1) == '\n') {
-				version[i] = '\0';
-				fptr += 2;
-				break;
-			}
-			else {
-				log_error("Error parsing http header: cr not followed by lf for uri: %s", uri->data);
-				buf_dealloc(uri);
-				return HTTP_BAD_REQUEST;
-			}
-		}
-		version[i] = *fptr++;
-	}
-	if(i == MAX_VERSION_LEN) {
-		version[i - 1] = '\0';
-		log_error("Error parsing http header: version too long for uri: %s - version: %s", uri->data, version);
-		buf_dealloc(uri);
-		return HTTP_BAD_REQUEST;
-	}
-	*ptr = fptr;
-	return 0;
+    int i;
+    buf_s *uri = &req->uri;
+    char *fptr = *ptr;
+    char *method = req->method;
+    char *version = req->version;
+
+    for(i = 0; i < MAX_METHOD_LEN; i++) {
+        if(*fptr == ' ') {
+            method[i] = '\0';
+            if(!http_check_method(method)) {
+                log_error("HTTP Unsupported Request method: %s", method);
+                return HTTP_UNSUPPORTED_METHOD;
+            }
+            fptr++;
+            break;
+        }
+        method[i] = *fptr++;
+    }
+    if(i == MAX_METHOD_LEN) {
+        method[i - 1] = '\0';
+        log_error("Request Method too long: %s", method);
+        return HTTP_BAD_REQUEST;
+    }
+    buf_init(uri);
+    for(i = 0; i < MAX_URI_LEN; i++) {
+        if(*fptr == ' ') {
+            buf_add_char(uri, '\0');
+            fptr++;
+            break;
+        }
+        buf_add_char(uri, *fptr);
+        fptr++;
+    }
+    if(i == MAX_URI_LEN) {
+        buf_add_char(uri, '\0');
+        log_error("URI too long: %s", uri->data);
+        return HTTP_BAD_REQUEST;
+    }
+    for(i = 0; i < MAX_VERSION_LEN; i++) {
+        if(*fptr == '\r') {
+            if(*(fptr + 1) == '\n') {
+                version[i] = '\0';
+                fptr += 2;
+                break;
+            }
+            else {
+                log_error("Error parsing http header: cr not followed by lf for uri: %s", uri->data);
+                buf_dealloc(uri);
+                return HTTP_BAD_REQUEST;
+            }
+        }
+        version[i] = *fptr++;
+    }
+    if(i == MAX_VERSION_LEN) {
+        version[i - 1] = '\0';
+        log_error("Error parsing http header: version too long for uri: %s - version: %s", uri->data, version);
+        buf_dealloc(uri);
+        return HTTP_BAD_REQUEST;
+    }
+    *ptr = fptr;
+    return 0;
 }
 
 bool http_check_method(const char *method) {
-	size_t i, len;
+    size_t i, len;
 
-	len = sizeof(supported_methods)/sizeof(char *);
-	for(i = 0; i < len; i++) {
-		if(!strcmp(supported_methods[i], method))
-			return true;
-	}
-	return false;
+    len = sizeof(supported_methods)/sizeof(char *);
+    for(i = 0; i < len; i++) {
+        if(!strcmp(supported_methods[i], method))
+            return true;
+    }
+    return false;
 }
 
 void http_reqest_header_dealloc(http_request_s *req) {
-	buf_dealloc(&req->uri);
-	buf_dealloc(&req->uri);
-	map_dealloc(&req->headers);
+    buf_dealloc(&req->uri);
+    buf_dealloc(&req->uri);
+    map_dealloc(&req->headers);
 }
 
 int http_do_request(http_request_s *req) {
-	uri_s uri;
-	
+    uri_s uri;
+
 }
 
 
 int http_resolve_address(const char *host_name, char *address_buffer) {
-	int result;
-	
-	result = db_check_dns_cache(host_name, address_buffer);
+    int result;
 
-	return 0;
+    result = db_check_dns_cache(host_name, address_buffer);
+
+    return 0;
 }
 
 /*
@@ -195,7 +195,7 @@ int http_parse_uri(uri_s *uri, const char *raw) {
     int result;
     uri_tok_s *tok;
     uri_toklist_s list = {NULL, NULL};    
-    
+
     result = http_lex_uri(&list, raw);
     if(result == -1) {
         log_error("Lexical Analysis on URI %s failed in %s()", raw, __func__);
@@ -265,6 +265,9 @@ void http_add_token(uri_toklist_s *list, char *lex, size_t lex_len, uri_tok_type
 }
 
 int http_syntax_uri(uri_s *uri, uri_toklist_s *list) {
-    
+
+
+
+
 }
 
